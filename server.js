b5ae404afaf50ad4bcc0b7870e557529168ea1d1
SussyBakaOn144Hz <dumbestshit10@gmail.com>
2026-04-07 18:16:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,6 @@ app.post("/v1/chat/completions", async (req, res) => {
         messages: finalMessages,
         stream: true,
         max_tokens: 4096,
-        reasoning_budget: 2048,
     chat_template_kwargs: {"enable_thinking":true}
       },
       {
